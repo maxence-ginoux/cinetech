5adf0e8345073e2d525169ed4df49d6d268377f5
maxence.ginoux <maxence.ginoux@laplateforme.io>
2024-04-22 13:15:35 +0200

    stockage des données de l'API en local Storage

--- api.js
+++ api.js
@@ -1,24 +1,21 @@
 const apiKey = '8c4b867188ee47a1d4e40854b27391ec';
-const movieId = '550'; // Exemple: ID du film "Fight Club"
 
-// URL de l'API TMDb
+// URL de l'API TMDb pour récupérer tous les films
 const url = `https://api.themoviedb.org/3/discover/movie?api_key=${apiKey}`;
 
-// Effectuer une requête GET à l'API TMDb
+// Récupérer les données depuis l'API
 fetch(url)
   .then(response => {
-    // Vérifier le statut de la réponse
     if (!response.ok) {
       throw new Error('La requête a échoué avec le code de statut ' + response.status);
     }
-    // Convertir la réponse en JSON
     return response.json();
   })
   .then(data => {
-    // Afficher les données de la réponse
-    console.log(data);
+    // Stocker les données dans localStorage
+    localStorage.setItem('moviesData', JSON.stringify(data.results));
+    console.log('Données des films stockées avec succès dans localStorage.');
   })
   .catch(error => {
-    // Gestion des erreurs
-    console.error('Une erreur s\'est produite:', error);
+    console.error('Une erreur s\'est produite lors de la récupération des données:', error.message);
   });
